73bfad066e29c371e0c6714342aed5690c881ff2
Tomer Ram <ramtomer0@gmail.com>
2019-01-27 21:02:16 +0200

    Added player movemet, run/walk and diagonal slowdown

---
+++ Nocturnal Snacktime/Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed;
+    public float diagonalMoveModifier;
+    public bool isRunning = false;
+    private float currentSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the player is holding down the Run button, he will run
+        if (Input.GetButtonDown("Run"))
+        {
+            isRunning = true;
+        }
+        //Else, he will walk
+        else if (Input.GetButtonUp("Run"))
+        {
+            isRunning = false;
+        }
+
+        //Walk
+        if (isRunning == false)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal") * currentSpeed * Time.deltaTime;
+            float moveVertical = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
+            transform.Translate(new Vector3(moveHorizontal, moveVertical));
+        }
+        //Run
+        else
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal") * (currentSpeed + 3) * Time.deltaTime;
+            float moveVertical = Input.GetAxis("Vertical") * (currentSpeed + 3) * Time.deltaTime;
+            transform.Translate(new Vector3(moveHorizontal, moveVertical));
+        }
+
+        //If moving diagonal, move at lower speed (modifier is changed in inspector)
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+        {
+            currentSpeed = moveSpeed * diagonalMoveModifier;
+        }
+        else
+        {
+            currentSpeed = moveSpeed;
+        }
+    }
+}
