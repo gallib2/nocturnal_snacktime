102d657ae21c892cc679bb4285143074d48c710e
Gal <gallib2@gmail.com>
2019-02-09 17:47:15 +0200

    create receipt

---
+++ Nocturnal Snacktime/Assets/Scripts/MenuBackpack.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBackpack : MonoBehaviour
+{
+    public bool isMenuOpen = true;
+    private GameObject[] menueItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menueItems = GameObject.FindGameObjectsWithTag("MenuItem");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMenuClicked()
+    {
+        Debug.Log("enter the funcitons");
+
+        if(isMenuOpen == true)
+        {
+            Debug.Log("close the menue");
+
+            Debug.Log("num of items" + menueItems.Length);
+
+            for (int i = 0; i < menueItems.Length; i++)
+            {
+                menueItems[i].SetActive(false);
+            }
+
+            isMenuOpen = false;
+        }
+        else
+        {
+            Debug.Log("menu open");
+
+            for (int i = 0; i < menueItems.Length; i++)
+            {
+                menueItems[i].SetActive(true);
+            }
+
+            isMenuOpen = true;
+        }
+    }
+}
---
+++ Nocturnal Snacktime/Assets/Scripts/Reciept.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reciept : MonoBehaviour
+{
+    public Text recieptText;
+
+    public Image[] recieptsItmes;
+    public int recieptSize;
+
+    private Vector2 initialPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //recieptText.text = " 1 X Cheery";
+
+        //initialPosition = new Vector2(-82, 100);
+        //int offsetY = 6;
+
+        //for (int i = 0; i < recieptsItmes.Length; i++)
+        //{
+        //   // recieptsItmes[i].transform.position = new Vector2(initialPosition.x, initialPosition.y + offsetY);
+        //    Vector2 position = new Vector2(initialPosition.x, initialPosition.y + offsetY);
+        //    Instantiate(recieptsItmes[i], position, Quaternion.identity);
+
+        //    recieptsItmes[i].transform.parent = (gameObject.transform);
+        //}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
