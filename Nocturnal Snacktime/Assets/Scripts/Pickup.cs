c606d76b002b679fe606e6ec7fc1abea85c83676
Gal <gallib2@gmail.com>
2019-02-09 15:27:45 +0200

    create slots inventory and collectable items

--- Nocturnal Snacktime/Assets/Scripts/Pickup.cs
+++ Nocturnal Snacktime/Assets/Scripts/Pickup.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     private Inventory inventory;
+    public GameObject itemButton;
 
     // Start is called before the first frame update
     void Start()
@@ -12,9 +13,21 @@ public class Pickup : MonoBehaviour
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    // Item can be added to inventory
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
     }
 }
